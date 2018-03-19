c5d3dc89df1b4727fa68c1337e16a12150c9df27
JolantaKapelanczyk <kola125@wp.pl>
2018-03-19 19:33:50 +0100

    wer

--- weryfikacja_uzytkownika.php
+++ weryfikacja_uzytkownika.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Do ćwiczeń utwórz bazę danych o nazwie user z tabelą  urzytkownik(pola tabeli: login, haslo)
+Do ćwiczeń utwórz bazę danych o nazwie user z tabelą  uzytkownik(pola tabeli: login, haslo)
 */
-echo '<form action="weryfikacja_urzytkownika.php" method="POST">
+echo '<form action="weryfikacja_uzytkownika.php" method="POST">
       Login: <input type="text"  name="log"><br /><br />
 	  Hasło: <input type="password" name ="has"><br /><br />
 	  <input type ="submit" name="loguj" value="ZALOGUJ">
@@ -16,14 +16,14 @@ echo '<form action="weryfikacja_urzytkownika.php" method="POST">
 		$pol=new mysqli('localhost','root','','user');
 
    
-			$dane=$pol->query("SELECT * FROM  urzytkownik ");
+			$dane=$pol->query("SELECT * FROM  uzytkownik ");
            $status=0;
 		   while( $ciag1=$dane->fetch_assoc())
 		   {
 			if($ciag1['login']==$log)
 			{
 				if(password_verify($has,$ciag1['haslo'])){
-					echo 'urzytkownik istnieje w bazie danych <br />';
+					echo 'uzytkownik istnieje w bazie danych <br />';
 					echo "login: ".$ciag1['login'];	
 					$status=1;
 				}
@@ -32,8 +32,8 @@ echo '<form action="weryfikacja_urzytkownika.php" method="POST">
 			if($status==0)
 			{
 			$ciag2=password_hash($has,PASSWORD_DEFAULT);
-			$wpisz = $pol->query("INSERT INTO urzytkownik SET login='$log', haslo='$ciag2'");
-			echo 'urzytkownik dodany do bazy<br />'; 
+			$wpisz = $pol->query("INSERT INTO uzytkownik SET login='$log', haslo='$ciag2'");
+			echo 'uzytkownik dodany do bazy<br />'; 
 			}
 		   
         mysqli_close($pol);
